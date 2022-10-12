f3b3073a7fd11e2420e031d3dbfe965adb33660d
Thomas Gollenia <thomas@gollenia.at>
2022-10-12 13:03:04 +0200

    Outsourced into Controller

--- lib/plugins/bible/ArticleController.php
+++ lib/plugins/bible/ArticleController.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace dokuwiki\plugins\bible;
-
+namespace dokuwiki\plugin\bible;
 
 
 class ArticleController
@@ -10,14 +9,15 @@ class ArticleController
 	public static function get()
 	{
 		global $INPUT;
-		$book = $INPUT->str('book', '10');
-		$chapter = $INPUT->str('chapter', 1);
-		$verse = $INPUT->str('verse', 0);
-		$lang = $INPUT->str('lang', 'de');
+		global $conf;
+		$book = $INPUT->int('book', '10');
 
-		$book = Book::find($book, $lang);
+		$chapter = $INPUT->int('chapter', 1);
+		$verse = $INPUT->int('verse', 0);
 
-		$pages = Article::where($book, $chapter);
+		$book = Book::find($book);
+
+		$pages = Article::where($book, $chapter, $INPUT->bool('pages', false));
 		header("Access-Control-Allow-Origin: *");
 		header('Content-Type: application/json');
 		echo json_encode($pages);
@@ -25,9 +25,62 @@ class ArticleController
 
 	public static function post()
 	{
+		$error = [];
+
+		global $INPUT;
+		$book = $INPUT->int('book', 0);
+		$chapter = $INPUT->str('chapter', 0);
+		$id = $INPUT->str('id', 0);
+		$lang = $INPUT->str('lang', '');
+
+		if (empty($lang)) $error[] = 'language missing';
+		if ($book == 0) $error[] = 'book missing';
+		if (empty($id)) $error[] = 'id missing';
+
+		if (!empty($error)) {
+			http_response_code(400);
+			echo json_encode(['error' => join(', ', $error)]);
+			return;
+		}
+
+		if (!auth_quickaclcheck($id) >= AUTH_EDIT) {
+			http_response_code(403);
+			echo json_encode(['error' => 'forbidden']);
+			return;
+		}
+
+		$book = Book::find($book, $lang);
+
+		$result = Article::add($book, $id, $chapter);
+		echo json_encode($result);
 	}
 
 	public static function delete()
 	{
+		$error = [];
+
+		global $INPUT;
+		$lang = $INPUT->str('lang', '');
+		$id = $INPUT->int('id', 0);
+
+		if (empty($lang)) $error[] = 'language missing';
+
+		if (empty($id)) $error[] = 'id missing';
+
+		if (!empty($error)) {
+			http_response_code(400);
+			echo json_encode(['error' => join(', ', $error)]);
+			return;
+		}
+
+		if (!auth_quickaclcheck($id) >= AUTH_EDIT) {
+			http_response_code(403);
+			echo json_encode(['error' => 'forbidden']);
+			return;
+		}
+
+		$result = Article::remove($id);
+		if (!$result) http_response_code(500);
+		echo json_encode($result);
 	}
 }
--- lib/plugins/bible/action.php
+++ lib/plugins/bible/action.php
@@ -4,22 +4,17 @@ use dokuwiki\plugin\bible\Article;
 use dokuwiki\plugin\bible\Bible;
 use dokuwiki\plugin\bible\Book;
 use dokuwiki\plugin\bible\Verse;
-use dokuwiki\plugins\bible\ArticleController;
+use dokuwiki\plugin\bible\ArticleController;
 
 class action_plugin_bible extends \dokuwiki\Extension\ActionPlugin
 {
 
 	function register(Doku_Event_Handler $controller)
 	{
-		$controller->register_hook(
-			"DOKUWIKI_INIT_DONE",
-			"AFTER",
-			$this,
-			"loadModels",
-		);
+
 
 		$controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, '_get_bible');
-		$controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, '_get_articles');
+		$controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, '_biblepages');
 		$controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, '_set_article');
 		$controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, '_delete_article');
 		//$controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'load_js');
@@ -45,7 +40,7 @@ class action_plugin_bible extends \dokuwiki\Extension\ActionPlugin
 		echo json_encode($verses);
 	}
 
-	function _get_articles(Doku_Event $event, $param)
+	function _biblepages(Doku_Event $event, $param)
 	{
 		if ($event->data !== 'biblepages') return;
 		$event->stopPropagation();
@@ -55,83 +50,16 @@ class action_plugin_bible extends \dokuwiki\Extension\ActionPlugin
 		global $_SERVER;
 		switch ($_SERVER['REQUEST_METHOD']) {
 			case 'GET':
-				BiblePages::get($INPUT);
+				ArticleController::get($INPUT);
+				break;
 			case 'POST':
-				BiblePages::post($INPUT);
+				ArticleController::post($INPUT);
+				break;
 			case 'DELETE':
-				BiblePages::delete($INPUT);
-		}
-	}
-
-	function _set_article(Doku_Event $event, $param)
-	{
-		if ($event->data !== 'biblepage') return;
-		$event->stopPropagation();
-		$event->preventDefault();
-
-		$error = [];
-
-		global $INPUT;
-		$book = $INPUT->int('book', 0);
-		$chapter = $INPUT->str('chapter', 0);
-		$id = $INPUT->str('id', 0);
-		$lang = $INPUT->str('lang', '');
-
-		if (empty($lang)) $error[] = 'language missing';
-		if ($book == 0) $error[] = 'book missing';
-		if (empty($id)) $error[] = 'id missing';
-
-		if (!empty($error)) {
-			http_response_code(400);
-			echo json_encode(['error' => join(', ', $error)]);
-			return;
+				ArticleController::delete($INPUT);
+				break;
 		}
 
-		if (!auth_quickaclcheck($id) >= AUTH_EDIT) {
-			http_response_code(403);
-			echo json_encode(['error' => 'forbidden']);
-			return;
-		}
-
-		$book = Book::find($book, $lang);
-
-		$result = Article::add($book, $id, $chapter);
-		echo json_encode($result);
-	}
-
-	function _delete_article(Doku_Event $event, $param)
-	{
-		if ($event->data !== 'plugin_bible_delete_article') return;
-		$event->stopPropagation();
-		$event->preventDefault();
-
-		$error = [];
-
-		global $INPUT;
-		$book = $INPUT->int('book', 0);
-		$lang = $INPUT->str('lang', '');
-		$id = $INPUT->int('id', 0);
-
-		if (empty($lang)) $error[] = 'language missing';
-		if ($book == 0) $error[] = 'book missing';
-		if (empty($id)) $error[] = 'id missing';
-
-		if (!empty($error)) {
-			http_response_code(400);
-			echo json_encode(['error' => join(', ', $error)]);
-			return;
-		}
-
-		if (!auth_quickaclcheck($id) >= AUTH_EDIT) {
-			http_response_code(403);
-			echo json_encode(['error' => 'forbidden']);
-			return;
-		}
-
-		$book = Book::find($book, $lang);
-
-		$result = Article::remove($book, $id);
-		if (!$result) http_response_code(500);
-		echo json_encode($result);
+		return;
 	}
 }
