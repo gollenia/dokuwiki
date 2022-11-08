0326261e0c20a61db420b0ef5a1aa5d2495bcb46
Thomas Gollenia <thomas@gollenia.at>
2022-11-08 17:30:29 +0100

    Console log removed

--- lib/tpl/dokuwiki/lib/Twig/Renderer.php
+++ lib/tpl/dokuwiki/lib/Twig/Renderer.php
@@ -9,39 +9,40 @@ use Contexis\Twig\Colors;
 use Twig\Extra\String\StringExtension;
 
 use Twig\{
-    Loader\FilesystemLoader,
-    Environment
+	Loader\FilesystemLoader,
+	Environment
 };
 
 
-class Renderer {
+class Renderer
+{
 
-    
 
-    static function compile($filenames, $data, $options = []) {
-        global $conf;
-        $loader = new FilesystemLoader(tpl_incdir() . './templates');
-        $twig = new Environment($loader, $options);
-        $twig->addExtension(new StringExtension());
-        CustomFunctions::register($twig);
-        CustomFilters::register($twig);
-        Colors::add_twig_filter($twig);
-        if($conf['allowdebug']) {
-            \Contexis\Core\Utilities\Debug::to_browser_console($data);
-        }
-        return $twig->render($filenames, $data);
-    }
 
-    static function compile_string(string $template, array $data) {
-        $loader = new \Twig\Loader\ArrayLoader([
-            'index.html' => $template,
-        ]);
-        
-        $twig = new Environment($loader);
-        $twig->addExtension(new StringExtension());
-        CustomFunctions::register($twig);
-        CustomFilters::register($twig);
-        
-        return $twig->render('index.html', $data);
-    }
+	static function compile($filenames, $data, $options = [])
+	{
+		global $conf;
+		$loader = new FilesystemLoader(tpl_incdir() . './templates');
+		$twig = new Environment($loader, $options);
+		$twig->addExtension(new StringExtension());
+		CustomFunctions::register($twig);
+		CustomFilters::register($twig);
+		Colors::add_twig_filter($twig);
+
+		return $twig->render($filenames, $data);
+	}
+
+	static function compile_string(string $template, array $data)
+	{
+		$loader = new \Twig\Loader\ArrayLoader([
+			'index.html' => $template,
+		]);
+
+		$twig = new Environment($loader);
+		$twig->addExtension(new StringExtension());
+		CustomFunctions::register($twig);
+		CustomFilters::register($twig);
+
+		return $twig->render('index.html', $data);
+	}
 }
