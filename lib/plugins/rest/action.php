7bfcc54987df60212b05ae26c56b778850442eee
Thomas Gollenia <thomas@gollenia.at>
2022-11-11 14:15:08 +0100

    tag oindexer added

--- lib/plugins/rest/action.php
+++ lib/plugins/rest/action.php
@@ -1,7 +1,8 @@
 <?php
 
 use Contexis\Core\Controller;
-
+use Contexis\Database\Tag;
+use dokuwiki\Extension\Event;
 
 class action_plugin_rest extends \dokuwiki\Extension\ActionPlugin
 {
@@ -13,6 +14,10 @@ class action_plugin_rest extends \dokuwiki\Extension\ActionPlugin
 			$this,
 			"loadModels",
 		);
+
+		$controller->register_hook('INDEXER_PAGE_ADD', 'BEFORE', $this, 'addTagsToIndex', array());
+		$controller->register_hook('INDEXER_PAGE_ADD', 'BEFORE', $this, 'addCategoriesToIndex', array());
+		$controller->register_hook('INDEXER_PAGE_ADD', 'BEFORE', $this, 'addAudienceToIndex', array());
 	}
 
 	function loadModels($event)
@@ -26,5 +31,29 @@ class action_plugin_rest extends \dokuwiki\Extension\ActionPlugin
 		require_once(__DIR__ . '/Models/Tag.php');
 		require_once(__DIR__ . '/Models/Category.php');
 		require_once(__DIR__ . '/Models/Audience.php');
+	}
+
+	public function addTagsToIndex(Event $event, $param)
+	{
+		// make sure the tags are cleaned and no duplicate tags are added to the index
+		$tags = p_get_metadata($event->data['page'], 'subject');
+		if (!is_array($tags)) {
+			$event->data['metadata']['subject'] = array();
+			return;
+		}
+
+		$event->data['metadata']['subject'] = Tag::cleanTagList($tags);
+	}
+
+	public function addCategoriesToIndex(Event $event, $param)
+	{
+		$category = p_get_metadata($event->data['page'], 'category');
+		$event->data['metadata']['category'] = $category;
+	}
+
+	public function addAudienceToIndex(Event $event, $param)
+	{
+		$audience = p_get_metadata($event->data['page'], 'audience');
+		$event->data['metadata']['audience'] = $audience;
 	}
 }
