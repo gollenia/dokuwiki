53ff1cdd1b80c87c3f393d4960896e142fc49b6d
Thomas Gollenia <thomas@gollenia.at>
2022-12-13 11:24:24 +0100

    synonyms prepared

--- lib/plugins/rest/Models/Category.php
+++ lib/plugins/rest/Models/Category.php
@@ -6,6 +6,12 @@ use Contexis\Core\Utilities\Strings;
 
 class Category
 {
+	public string $value;
+	public string $label;
+	public string $icon;
+	public string $color;
+	public array $synonyms;
+
 	static function findAll($assoc = false)
 	{
 		if (!page_exists("system:categories")) return [];
@@ -17,5 +23,31 @@ class Category
 			$result[$tag['value']] = $tag;
 		}
 		return $result;
+	}
+
+	static function getAllSynonyms() {
+		if (!page_exists("system:categories")) return [];
+		$categories = Strings::json_to_array(rawWiki("system:categories"));
+		$result = [];
+		foreach($categories as $category) {
+			if(!array_key_exists('synonyms', $category)) continue;
+			array_merge($result, $categories['synonyms']);
+		}
+	}
+
+	static function getCategoryBySynonym($synonym) {
+		if (!page_exists("system:categories")) return [];
+		$categories = Strings::json_to_array(rawWiki("system:categories"));
+		$result = null;
+		foreach($categories as $category ) {
+			if(!array_key_exists('synonyms', $category)) continue;
+			if(array_search($synonym, $category['synonyms'])) {
+				$result = $category;
+				break;
+			}
+		}
+
+		return $result;
+
 	}
 }
