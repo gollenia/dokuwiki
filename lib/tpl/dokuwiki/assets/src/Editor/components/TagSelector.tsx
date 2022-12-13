39f75234d59ed16f453263fd0fe0afe721e4256d
Thomas Gollenia <thomas@gollenia.at>
2022-12-13 11:19:01 +0100

    multiple bigfixes

--- lib/tpl/dokuwiki/assets/src/Editor/components/TagSelector.tsx
+++ lib/tpl/dokuwiki/assets/src/Editor/components/TagSelector.tsx
@@ -1,115 +1,119 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Tag } from '../services/models/Site';
 
 interface TagSelectorProps {
-	availableTags: Array<Tag>
-	placeholder?: string
-	default?: string | number
-	savedTags?: Array<string>
-	onChange?: (option: Array<string>) => void
+    availableTags: Array<Tag>;
+    placeholder?: string;
+    default?: string | number;
+    tagList?: Array<string>;
+    onChange?: (option: Array<string>) => void;
 }
 
-const TagSelector: React.FC<TagSelectorProps> = (props) => {
+const TagSelector: React.FC<TagSelectorProps> = props => {
+    const { availableTags, onChange, placeholder, tagList } = props;
 
-	const { availableTags, onChange, placeholder, savedTags } = props;
-	
+    const input = useRef<HTMLInputElement>(null);
 
-	const input = useRef<HTMLInputElement>(null);
+    const [inputField, setInputField] = useState<string>('');
+    const [selection, setSelection] = useState<number>(-1);
+    const [listSelect, setListSelect] = useState<number>(-1);
 
-	const [inputField, setInputField] = useState<string>('');
-	const [selection, setSelection] = useState<number>(-1);
-	const [listSelect, setListSelect] = useState<number>(-1);
-	const [tagList, setTagList] = useState<Array<Tag>>([])
+    const dropdownSelect = (value: string, update = true) => {
+        const index = availableTags.findIndex(option => option.id == value);
+        setSelection(index);
+        setInputField('');
+        input.current.value = '';
+        onChange([...tagList, value]);
+    };
 
-	console.log(tagList)
+    const filteredOptions = () => {
+        const unselectedAvailableTags = availableTags.filter(tag => !tagList.includes(tag.id));
+        if (inputField.length === 0) return unselectedAvailableTags;
 
-	useEffect(() => {
-		
-		const filteredTags = savedTags.filter((tag) => {
-			return availableTags.find((i) => i.id == tag)
-		}).map((tag) => {
-			return availableTags.find((i) => i.id == tag)
-		})
+        return unselectedAvailableTags.filter(option => {
+            return option.name.toLocaleLowerCase().includes(inputField.toLowerCase());
+        });
+    };
 
-		setTagList(filteredTags)
-		
-	}, [savedTags])
+    const keyPress = (event: any) => {
+        if (event.key == 'ArrowDown') {
+            setListSelect(listSelect + 1);
+        }
+        if (event.key == 'ArrowUp' && listSelect != -1) setListSelect(listSelect - 1);
+        if (event.key == 'Enter') {
+            dropdownSelect(filteredOptions()[listSelect].id);
+            input.current.value = '';
+        }
+        if (event.key == 'Escape') {
+            setListSelect(-1);
+            input.current?.blur();
+        }
+    };
 
-	const dropdownSelect = (value: string | number, update = true) => {
-		const index = availableTags.findIndex((option) => option.id == value)
-		setSelection(index);
-		setInputField("");
-		input.current.value = "";
+    const removeTag = (index: number) => {
+        tagList.splice(index, 1);
+        onChange(tagList);
+    };
 
-		console.log("adding", availableTags[index], "to", tagList)
-		setTagList(tags => [...tags, availableTags[index]])
-		if (onChange && update) {
-			const tags = [...tagList, availableTags[index]].map((tag) => tag.id)
-			console.log("tag", tags)
-			onChange(tags)
-		}
-	}
-
-	console.log(availableTags)
-
-	const filteredOptions = () => {
-		if (inputField.length === 0) return availableTags;
-		if (inputField.slice(-1) == "*") {
-			return availableTags.filter((option) => option.name.startsWith(inputField.slice(0, -1).toLowerCase()))
-		}
-		return availableTags.filter((option) => option.name.includes(inputField.toLowerCase()))
-	}
-
-	const keyPress = (event: any) => {
-		console.log(event)
-		if (event.key == "ArrowDown") {
-			console.log('down')
-			setListSelect(listSelect + 1)
-		}
-		if (event.key == "ArrowUp" && listSelect != -1) setListSelect(listSelect - 1)
-		if (event.key == "Enter") {
-			dropdownSelect(filteredOptions()[listSelect].id);
-		}
-		if (event.key == "Escape") {
-			setListSelect(-1);
-			input.current?.blur();
-		}
-	}
-
-	const removeTag = (index: number) => {
-		setTagList((tags) => { return tags.splice(index,1)})
-	}
-
-
-	return (
-		<div>
-			<div className="tagList tags mb-4 mt-2"> { tagList.map((tagList, index) => {
-				return <span className='badge tag bg-primary'>{tagList.name}<i className="material-symbols-outlined" onClick={() => {removeTag(index)}}>cancel</i></span>} ) } 
-			</div>
-			<label>Schlagworte hinzufügen</label>
-			<div className='combobox' onKeyDown={(event) => keyPress(event)}>
-				
-				<input className="form-control" ref={input} type="text" onMouseOver={() => { setListSelect(-1) }} onClick={(event) => { }} placeholder={selection != -1 ? availableTags[selection].id : placeholder} value={inputField} onChange={(event) => setInputField(event.target.value)} />
-				<ul>
-					{filteredOptions().map((option, index) => {
-						return <li className={listSelect == index ? "selected" : ''} onMouseDown={(event) => { dropdownSelect(option.id) }} key={index}>
-							{option.name}
-						</li>
-					})}
-					{filteredOptions().length == 0 &&
-						<li className="muted">No Result</li>
-					}
-				</ul>
-		</div>
-		</div>
-		
-	)
-}
+    return (
+        <div>
+            <div className="tagList tags mb-4 mt-2">
+                {' '}
+                {tagList.map((tagId, index) => {
+                    const tag = availableTags.find(item => item.id == tagId);
+                    if (!tag) return;
+                    return (
+                        <span className="badge tag bg-primary">
+                            {tag.name}
+                            <i
+                                className="material-symbols-outlined"
+                                onClick={() => {
+                                    removeTag(index);
+                                }}
+                            >
+                                cancel
+                            </i>
+                        </span>
+                    );
+                })}
+            </div>
+            <label>Schlagworte hinzufügen</label>
+            <div className="combobox" onKeyDown={event => keyPress(event)}>
+                <input
+                    className="form-control"
+                    ref={input}
+                    type="text"
+                    onMouseOver={() => {
+                        setListSelect(-1);
+                    }}
+                    onClick={event => {}}
+                    //placeholder={selection != -1 ? availableTags[selection].id : placeholder}
+                    value={inputField}
+                    onChange={event => setInputField(event.target.value)}
+                />
+                <ul>
+                    {filteredOptions().map((option, index) => {
+                        return (
+                            <li
+                                className={listSelect == index ? 'selected' : ''}
+                                onMouseDown={event => {
+                                    dropdownSelect(option.id);
+                                }}
+                                key={index}
+                            >
+                                {option.name}
+                            </li>
+                        );
+                    })}
+                    {filteredOptions().length == 0 && <li className="muted">No Result</li>}
+                </ul>
+            </div>
+        </div>
+    );
+};
 
 TagSelector.defaultProps = {
-	placeholder: '',
-	savedTags: [],
+    placeholder: '',
+};
 
-}
-
-export default TagSelector
+export default TagSelector;
