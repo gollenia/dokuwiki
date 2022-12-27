0fd68188872a722e4695e52e8de9e910a39e7942
Thomas Gollenia <thomas@gollenia.at>
2022-12-27 08:30:02 +0100

    logout route

--- lib/tpl/dokuwiki/conf/routes.php
+++ lib/tpl/dokuwiki/conf/routes.php
@@ -1,8 +1,12 @@
 <?php
+
 global $ID;
 global $INPUT;
 global $ACT;
+
+
 return [
+	"\\Contexis\\Controllers\\Logout" => $ACT == "logout",
 	"\\Contexis\\Controllers\\Page" => array_key_exists("controller", $_GET) && $_GET['controller'] === 'page',
 	"\\Contexis\\Controllers\\Pages" => array_key_exists("controller", $_GET) && $_GET['controller'] === 'pages',
 	"\\Contexis\\Controllers\\Dashboard" => $ID == "dashboard",
