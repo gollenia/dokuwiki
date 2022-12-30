38d3a358bfb3e8680c24ec248431f6201a8756ed
Thomas Gollenia <thomas@gollenia.at>
2022-12-30 14:01:36 +0100

    color and padding added

--- lib/tpl/dokuwiki/assets/src/Editor/components/Dialogs/Box.tsx
+++ lib/tpl/dokuwiki/assets/src/Editor/components/Dialogs/Box.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 interface Props {
+    content?: string;
     showBox: boolean;
     setShowBox: (show: boolean) => void;
     onChange: (boxSyntax: string) => void;
@@ -8,29 +9,47 @@ interface Props {
 
 export type Box = {
     background?: string;
-    title?: string;
+    color?: string;
     inline: boolean;
+    padding?: number;
+    marginTop?: number;
+    marginBottom?: number;
 };
 
 const Box = (props: Props) => {
     const { showBox, onChange, setShowBox } = props;
     const [boxObject, setBoxObject] = useState<Box>({
         background: '#eeeeee',
-        title: '',
         inline: false,
+        padding: 1.5,
     });
 
     const [error, setError] = useState<string>('');
 
     const createSyntax = () => {
-        return `<box title="${boxObject.title}" inline="${boxObject.inline ? 1 : 0}" background="${
-            boxObject.background
-        }">`;
+        let box = '<box ';
+        if (boxObject.padding) box += `padding="${boxObject.padding}rem" `;
+        if (boxObject.inline) box += 'inline="1" ';
+        if (boxObject.color) box += `color="${boxObject.color}" `;
+        if (boxObject.background) box += `background="${boxObject.background}" `;
+        box = box.trim() + '>';
+        box += boxObject.inline ? '' : '\n';
+
+        return box;
     };
 
     const clickOk = () => {
         onChange(createSyntax());
         setShowBox(false);
+    };
+
+    const exampleStyle = {
+        backgroundColor: boxObject.background,
+        color: boxObject.color,
+        paddingTop: boxObject.inline ? '3px' : boxObject.padding + 'rem',
+        paddingBottom: boxObject.inline ? '3px' : boxObject.padding + 'rem',
+        paddingLeft: boxObject.inline ? '5px' : boxObject.padding + 'rem',
+        paddingRight: boxObject.inline ? '5px' : boxObject.padding + 'rem',
     };
 
     return (
@@ -59,22 +78,7 @@ const Box = (props: Props) => {
                             ></button>
                         </div>
                         <div className="container">
-                            <div className="mb-4">
-                                <label>Title</label>
-                                <input
-                                    className={'form-control' + (error ? ' is-invalid' : '')}
-                                    type="text"
-                                    value={boxObject.title}
-                                    onChange={event =>
-                                        setBoxObject(box => {
-                                            return { ...box, title: event.target.value };
-                                        })
-                                    }
-                                />
-                                {error && <div className="invalid-feedback">{error}</div>}
-                            </div>
-
-                            <div className="d-flex align-items-center mb-4">
+                            <div className="d-flex align-items-center mb-4 gap-4">
                                 <input
                                     className={'form-control' + (error ? ' is-invalid' : '')}
                                     type="color"
@@ -85,9 +89,44 @@ const Box = (props: Props) => {
                                         })
                                     }
                                 />
-                                <label>Farbe</label>
+                                <label>Hintergrundfarbe</label>
                                 {error && <div className="invalid-feedback">{error}</div>}
                             </div>
+                            <div className="d-flex align-items-center mb-4 gap-4">
+                                <input
+                                    className={'form-control' + (error ? ' is-invalid' : '')}
+                                    type="color"
+                                    value={boxObject.color}
+                                    onChange={event =>
+                                        setBoxObject(box => {
+                                            return { ...box, color: event.target.value };
+                                        })
+                                    }
+                                />
+                                <label>Textfarbe</label>
+                                {error && <div className="invalid-feedback">{error}</div>}
+                            </div>
+                            <div className="mb-4 d-flex align-items-center gap-4">
+                                <label htmlFor="padding-slider">Innenabstand</label>
+                                <input
+                                    className={'form-range' + (error ? ' is-invalid' : '')}
+                                    type="range"
+                                    max="4"
+                                    min="0"
+                                    step="0.5"
+                                    disabled={boxObject.inline}
+                                    id="padding-slider"
+                                    value={boxObject.padding}
+                                    onChange={event =>
+                                        setBoxObject(box => {
+                                            return { ...box, padding: parseFloat(event.target.value) };
+                                        })
+                                    }
+                                />
+                                <b>{boxObject.padding} rem</b>
+                                {error && <div className="invalid-feedback">{error}</div>}
+                            </div>
+
                             <div className="form-check mb-4">
                                 <input
                                     className={'form-check-input' + (error ? ' is-invalid' : '')}
@@ -101,9 +140,27 @@ const Box = (props: Props) => {
                                     }
                                 />
                                 <label className="form-check-label" htmlFor="checkInline">
-                                    Als Inline-Element behandeln
+                                    In den Fließtext einbinden
                                 </label>
                                 {error && <div className="invalid-feedback">{error}</div>}
+                            </div>
+
+                            <div className="mb-4">
+                                {boxObject.inline ? (
+                                    <p>
+                                        sit amet, consetetur sadipscing elitr,{' '}
+                                        <span style={exampleStyle}>sed diam nonumy</span> eirmod tempor invidunt ut
+                                        labore et dolore magna aliquyam erat, sed diam voluptua.
+                                    </p>
+                                ) : (
+                                    <div className="box-example" style={exampleStyle}>
+                                        <h3>Lorem ipsum dolor</h3>
+                                        <p>
+                                            sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                                            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                        </p>
+                                    </div>
+                                )}
                             </div>
                         </div>
                         <div className="modal-footer">
