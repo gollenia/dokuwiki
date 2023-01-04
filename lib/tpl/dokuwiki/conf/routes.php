eb81a7012f168a0cf197f0021bc482f9eddc0f3e
Thomas Gollenia <thomas@gollenia.at>
2023-01-04 15:01:33 +0100

    temp changes

--- lib/tpl/dokuwiki/conf/routes.php
+++ lib/tpl/dokuwiki/conf/routes.php
@@ -6,6 +6,7 @@ global $ACT;
 
 
 return [
+	//"\\Contexis\\Controllers\\OpenGraph" => substr($ID, 0,2) == "og",
 	"\\Contexis\\Controllers\\Logout" => $ACT == "logout",
 	"\\Contexis\\Controllers\\Page" => array_key_exists("controller", $_GET) && $_GET['controller'] === 'page',
 	"\\Contexis\\Controllers\\Pages" => array_key_exists("controller", $_GET) && $_GET['controller'] === 'pages',
