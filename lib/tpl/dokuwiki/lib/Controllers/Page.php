8f21fdd22efdab435a6e0174a804f247144c6702
Thomas Gollenia <thomas@gollenia.at>
2023-01-10 08:35:42 +0100

    added meta fn

--- lib/tpl/dokuwiki/lib/Controllers/Page.php
+++ lib/tpl/dokuwiki/lib/Controllers/Page.php
@@ -30,4 +30,22 @@ class Page extends Controller
 
 		return json_encode($page);
 	}
+
+	public function ajax_meta($request) {
+		global $ID;
+		header("Access-Control-Allow-Origin: *");
+		$filter = ['abstract', 'title', 'pageimage', 'tags'];
+		$id = $request->str('id', $ID);
+		$page = \dokuwiki\plugins\rest\Models\Page::find($id, $filter);
+		if (!$page->exists() && !$request->has('edit')) {
+			$page = \dokuwiki\plugins\rest\Models\Page::find('system:notfound');
+		}
+		$result = [
+			'title' => $page->title,
+			'abstract' => $page->abstract,
+			'pageimage' => $page->pageimage,
+			'tags' => $page->tags
+		];
+		return json_encode($result);
+	}
 }
