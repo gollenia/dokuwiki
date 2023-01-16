db105610b311751026ca3a5e4bbe783cf8574d8d
Thomas Gollenia <thomas@gollenia.at>
2023-01-16 16:01:44 +0100

    indexer bud removed

--- lib/plugins/rest/Helpers/Indexer.php
+++ lib/plugins/rest/Helpers/Indexer.php
@@ -316,7 +316,7 @@ class Index
 
 			if ($file == "start.txt" || substr($file, -4) != ".txt") continue;
 
-			if ($result && strcmp(end($result)['id'], $item['id']) !== 0) {
+			if ($item && strcmp(end($result)['id'], $item['id']) !== 0) {
 				$result[] = $item;
 			}
 		}
